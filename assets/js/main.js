e85087857115d2c196c6137bd5c221c5b4a740d5
Denny Setyawan Huda <dennshgit@gmail.com>
2022-09-29 19:33:35 +0700

    feat: add array questions

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let questions = [
+  {
+    question: "Apakah ini simbol dari HTML5?",
+    imgSrc: "/assets/img/html.png",
+    choiceA: "Benar",
+    choiceB: "Salah",
+    choiceC: "Salah",
+    correct: "A",
+  },
+  {
+    question: "Apakah ini simbol dari CSS3?",
+    imgSrc: "/assets/img/css.png",
+    choiceA: "Salah",
+    choiceB: "Benar",
+    choiceC: "Salah",
+    correct: "B",
+  },
+  {
+    question: "Apakah ini simbol dari JavaScript?",
+    imgSrc: "/assets/img/js.png",
+    choiceA: "Salah",
+    choiceB: "Salah",
+    choiceC: "Benar",
+    correct: "C",
+  },
+];
