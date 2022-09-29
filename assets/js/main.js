466b014fca0a823ce8d6342479f05aedaa501d13
Bayu Satrio Wibowo <bayusatriowid@gmail.com>
2022-09-29 19:42:18 +0700

    feat: add score render and percent answer

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,3 +107,26 @@ function answerIsCorrect() {
 function answerIsWrong() {
   document.getElementById(runningQuestion).style.backgroundColor = "#f00";
 }
+
+// score render
+function scoreRender() {
+  scoreDiv.style.display = "block";
+
+  // calculate the amount of question percent answered by the user
+  const scorePerCent = Math.round((100 * score) / questions.length);
+
+  // choose the image based on the scorePerCent
+  let img =
+    scorePerCent >= 80
+      ? "/assets/img/5.png"
+      : scorePerCent >= 60
+      ? "/assets/img/4.png"
+      : scorePerCent >= 40
+      ? "/assets/img/3.png"
+      : scorePerCent >= 20
+      ? "/assets/img/2.png"
+      : "/assets/img/1.png";
+
+  scoreDiv.innerHTML = "<img src=" + img + ">";
+  scoreDiv.innerHTML += "<p>" + scorePerCent + "%</p>";
+}
